d757ec72105136f13ab9800480238426adfdcd36
Daniel Oh <doh422@gmail.com>
2016-02-19 15:52:26 -0800

    players view page created

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var players = require('./players/players');
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.render('pages/index');
+});
+
+app.get('/players', function(req, res) {
+	res.render('pages/players', {
+		players: players.players
+	});
 });
 
 app.listen(port);

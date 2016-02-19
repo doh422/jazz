a653b3ed5ce4286d44810fd9d26115ae8f562839
Daniel Oh <doh422@gmail.com>
2016-02-19 15:45:20 -0800

    player model and seeds created

---
+++ players/players.js
@@ -0,0 +1,4 @@
+module.exports.players = [
+	{id: 1, name: 'Gordon Hayward', number: 20, position: 'SF', college: 'Butler', headshot: "http://www.standard.net/image/2014/10/27/1200x_a16-9_b0_q80_cTC/Gordon-Hayward.jpg"},
+	{id: 2, name: 'Derrick Favors', number: 15, position: 'PF', college: 'Georgia Tech', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4257.png&w=350&h=254"}
+];

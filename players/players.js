ddda0f2d304a4adffe751f688b5c9ea7e6dd4db0
Daniel Oh <doh422@gmail.com>
2016-02-23 14:16:49 -0800

    added player models

--- players/players.js
+++ players/players.js
@@ -1,4 +1,8 @@
 module.exports.players = [
-	{id: 1, name: 'Gordon Hayward', number: 20, position: 'SF', college: 'Butler', headshot: "http://www.standard.net/image/2014/10/27/1200x_a16-9_b0_q80_cTC/Gordon-Hayward.jpg"},
-	{id: 2, name: 'Derrick Favors', number: 15, position: 'PF', college: 'Georgia Tech', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4257.png&w=350&h=254"}
+	{id: 1, name: 'Gordon Hayward', number: 20, position: 'SF', college: 'Butler', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4249.png&w=350&h=254"},
+	{id: 2, name: 'Derrick Favors', number: 15, position: 'PF', college: 'Georgia Tech', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4257.png&w=350&h=254"},
+	{id: 3, name: 'Rudy Gobert', number: 27, position: 'C', college: 'France', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3032976.png&w=350&h=254"},
+	{id: 4, name: 'Rodney Hood', number: 5, position: 'SG', college: 'Duke', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2581177.png&w=350&h=254"},
+	{id: 5, name: 'Alec Burks', number: 10, position: 'SG', college: 'Colorado', headshot: "http://fsupdates.com/wp-content/uploads/2015/10/Alec-Burks.png"},
+	{id: 6, name: 'Trey Lyles', number: 44, position: 'PF', college: 'Kentucky', headshot: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3136196.png&w=350&h=254"}
 ];
